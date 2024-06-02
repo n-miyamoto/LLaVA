aff1c77d4f84ee7d785f36d7857c5321294cfccb
n-miyamoto <scotsdowneroad@gmail.com>
2024-06-02 11:53:55 +0900

    Temporary: use 4-bit quantized inference due to memory constraints

--- llava/eval/run_llava.py
+++ llava/eval/run_llava.py
@@ -53,7 +53,8 @@ def eval_model(args):
 
     model_name = get_model_name_from_path(args.model_path)
     tokenizer, model, image_processor, context_len = load_pretrained_model(
-        args.model_path, args.model_base, model_name
+        args.model_path, args.model_base, model_name,
+        load_8bit=False, load_4bit=True, device="cuda"
     )
 
     qs = args.query
